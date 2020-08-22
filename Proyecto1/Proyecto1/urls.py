03349eff050126dc322b54e24154a267fd1e78d1
LAPTOP-5PUCK7BU\euge_ <albrechteuge@gmail.com>
2020-08-22 09:57:13 -0300

    pagina recipes creada

--- Proyecto1/Proyecto1/urls.py
+++ Proyecto1/Proyecto1/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Proyecto1.views import saludo
+# from Proyecto1.views import saludo
 from Proyecto1.views import home
+from Proyecto1.views import recipes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('saludo/',saludo),
-    path('home/', home)
+#    path ('saludo/',saludo),
+    path('home/', home),
+    path('recipes/', recipes)
 ]
--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,6 +1,30 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 import datetime
+
+def home(request): #Inicio
+    nombre = "Juancito Perez"
+    ahora = datetime.datetime.now()
+    doc_externo = open("C:/IngenieriaWeb/Proyecto1/Proyecto1/plantillas/plantilla_home.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context({"nombre_persona": nombre, "hora_actual": ahora})
+    documento = plt.render(ctx)
+
+    return HttpResponse(documento)
+
+def recipes(request): #Página recetas - Necesita login
+    nombre = "Ana"
+    ahora = datetime.datetime.now()
+    doc_externo = open("C:/IngenieriaWeb/Proyecto1/Proyecto1/plantillas/plantilla.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context({"nombre_persona": nombre, "hora_actual": ahora})
+    documento = plt.render(ctx)
+
+    return HttpResponse(documento)
+
+"""
 def saludo(request): #Vista
     nombre="Juancito Perez"
     ahora=datetime.datetime.now()
@@ -11,15 +35,5 @@ def saludo(request): #Vista
     documento=plt.render(ctx)
 
     return HttpResponse(documento)
-
-def home(request): #Inicio
-    nombre = 'Euge'
-    ahora = datetime.datetime.now()
-    doc_externo = open("C:/IngenieriaWeb/Proyecto1/Proyecto1/plantillas/plantilla_home.html")
-    plt = Template(doc_externo.read())
-    doc_externo.close()
-    ctx = Context({"nombre_persona": nombre, "hora_actual": ahora})
-    documento = plt.render(ctx)
-
-    return HttpResponse(documento)
+"""
 
