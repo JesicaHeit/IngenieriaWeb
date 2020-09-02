7d678f5e73e278c6ed751cd4bf77d08d6fcd3975
ivangalindez <ivangalindez0@gmail.com>
2020-09-02 03:10:51 -0300

    Add files via upload

--- Proyecto1/users/forms.py
+++ Proyecto1/users/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from django.forms import ModelForm
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class CustomUserForm(UserCreationForm):
-        class Meta:  
-            model = User  
-            fields = ('username', 'email', 'password1', 'password2')
+	email = forms.EmailField(max_length=200, help_text='Required')
+	class Meta:  
+		model = User  
+		fields = ('username', 'email', 'password1', 'password2')
