9965b811857ba4030089f2dccdf8ab0517561fc7
JesicaHeit <51091834+JesicaHeit@users.noreply.github.com>
2020-09-05 20:05:28 -0300

    Update forms.py

--- Proyecto1/users/forms.py
+++ Proyecto1/users/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from captcha.fields import ReCaptchaField
 
 class CustomUserForm(UserCreationForm):
 	email = forms.EmailField(max_length=200, help_text='Required')
+	captcha=ReCaptchaField()
 	class Meta:  
 		model = User  
 		fields = ('username', 'email', 'password1', 'password2')
