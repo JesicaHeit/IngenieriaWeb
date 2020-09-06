3a13d060e7b591414285f7e3dc86d02305247f9b
JesicaHeit <51091834+JesicaHeit@users.noreply.github.com>
2020-09-06 20:46:09 -0300

    Update urls.py

--- Proyecto1/Proyecto1/urls.py
+++ Proyecto1/Proyecto1/urls.py
@@ -51,5 +51,5 @@ urlpatterns = [
     path('post/<int:pk>/', post_detail, name='post_detail'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
-    path('perfil/', ShowProfilePageView.as_view(), name='show_profile'),
+
 ]
