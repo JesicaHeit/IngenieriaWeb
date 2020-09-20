a86de6527cdb0da58e10b434a15dc248ef198761
JesicaHeit <51091834+JesicaHeit@users.noreply.github.com>
2020-09-20 11:38:08 -0300

    Update urls.py
    Funciona el Like y Unlike, termine de arrgelar la foto del perfil

--- Proyecto1/Proyecto1/urls.py
+++ Proyecto1/Proyecto1/urls.py
@@ -38,7 +38,7 @@ from recetas.views import post_list2
 from recetas.views import post_edit
 from recetas.views import post_detail2
 from recetas.views import borrar_receta
-from recetas.views import LikeView, UnLikeView
+from recetas.views import LikeView
 from users.views import UserEditView,ShowProfilePageView, show_profile
 
 urlpatterns = [
@@ -65,7 +65,6 @@ urlpatterns = [
     path('post/<int:pk>/edit/', post_edit, name='post_edit'),
     path('borrar/<int:pk>/', borrar_receta, name='borrar_receta'),
     path('like/<int:pk>', LikeView, name='like_post'),
-    path('unlike/<int:pk>', UnLikeView, name='unlike_post'),
     path('switch_follow/', follow_unfollow_profile, name ='follow_unfollow_profile'),
 
 
