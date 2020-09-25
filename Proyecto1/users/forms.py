9df2f88cbf07974a6b9f73a9497fb8c8c4bb49f0
JesicaHeit <51091834+JesicaHeit@users.noreply.github.com>
2020-09-25 07:51:00 -0300

    Update forms.py

--- Proyecto1/users/forms.py
+++ Proyecto1/users/forms.py
@@ -5,7 +5,9 @@ from captcha.fields import ReCaptchaField
 
 class CustomUserForm(UserCreationForm):
 	email = forms.EmailField(max_length=200, help_text='Required')
-	captcha=ReCaptchaField()
-	class Meta:  
-		model = User  
-		fields = ('username', 'email', 'password1', 'password2')
+	first_name = forms.CharField(label="Nombre", help_text='Required')
+	last_name = forms.CharField(label="Apellido", help_text='Required')
+	captcha = ReCaptchaField()
+	class Meta:
+		model = User
+		fields = ('username','first_name', 'last_name', 'email', 'password1', 'password2')
