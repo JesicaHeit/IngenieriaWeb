9dab0a55c950ce7bd938bb7a9cb856f5d04393f3
JesicaHeit <51091834+JesicaHeit@users.noreply.github.com>
2020-10-12 13:45:11 -0300

    Update signals.py

--- Proyecto1/users/signals.py
+++ Proyecto1/users/signals.py
@@ -4,6 +4,6 @@ from .models import Profile
 from django.contrib.auth.models import User
 
 @receiver(post_save, sender = User)
-def post_Save_create_profile(sender,instance,created,**kwargs)
+def post_Save_create_profile(sender,instance,created,**kwargs):
 	if created:
 		Profile.objects.create(user=instance)
